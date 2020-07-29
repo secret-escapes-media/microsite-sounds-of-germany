ee1b9d9b1996812d09b4a889e20a613cd6d28257
hamishgray <hamish.gray@secretescapes.com>
2020-07-29 11:39:53 +0100

    client review

--- _assets/js/main.js
+++ _assets/js/main.js
@@ -5,33 +5,9 @@ $(document).ready(function(){
     var node = $('.soundwave__node');
     $(node).each(function(){
       var height = Math.floor(Math.random() * (85 - 20 + 1)) + 20;
-      console.log(height);
       $(this).css('height',height+'%');
     });
   }, 500);
 });
 
 
-
-
-///////////////////////////////////////
-//    Cursor tracking icon
-///////////////////////////////////////
-
-// $(document).bind('mousemove', function(e){
-//   var height = $('#cursor.active').height();
-//   $('#cursor.active').css({
-//      left:  e.pageX+15,
-//      top:   e.pageY
-//   });
-// });
-//
-// $('.soundwave__node').hover(
-//   function() {
-//     var type = $(this).data('cursor-content');
-//     $('#cursor').addClass('active');
-//     $('#cursor').html(type);
-//   }, function() {
-//     $('#cursor').removeClass().empty();
-//   }
-// );
